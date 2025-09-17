0791a588d7bd2757f2d1c0c407b2e6776592f5af
Ajin Joseph <ajin.joseph@techtaliya.com>
2025-09-17 14:05:27 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,29 @@
+# In python variables are Dynamically typed variables
+x = 10
+y = 20
+z = x + y
+print(z)
+
+name = "Ajin Joseph" #String
+age = 30 #Integer
+price = 500.75 #Float
+isActive = True #Boolean
+
+print(name)
+print(age)
+print(price)
+print(isActive)
+
+# Variable Naming
+# 1.Variable names should start with letter or underscore (_)
+# 2.Variable names should not start with numbers
+# 3.Variable names can contain letters, numbers (alphanumeric)
+# valid examples
+_isValid = True
+initialValue = 0
+mark1 = 58
+
+# 1mark = 60  is an invalid variable name (starts with number)
+
+
+
